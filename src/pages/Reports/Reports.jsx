e509055b56659cd361190ba06d91549fa759c32b
Farid Guluzade <faridfg@code.edu.az>
2022-04-11 12:04:07 +0400

    hiding PriceList button, if user brand is Madeyra

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,28 +1,46 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import jwt from "jwt-decode";
 
 const menuItems = [
-    { title: 'Mənim Satışlarım', url: 'sales' },
-    { title: 'Qarşılıqlı Hesablaşmalar', url: '' },
-    { title: 'Anbar Qalığı', url: 'options' },
-    { title: 'Price List', url: 'pricelist' }
+    {title: 'Mənim Satışlarım', url: 'sales'},
+    {title: 'Qarşılıqlı Hesablaşmalar', url: ''},
+    {title: 'Anbar Qalığı', url: 'options'},
+    {title: 'Price List', url: 'pricelist'}
 ]
 
 const Reports = () => {
+    const [brand, setBrand] = useState("");
+
+    const getUser = () => {
+        const token = localStorage.getItem("jwt_token");
+        return token && jwt(token);
+    }
+
+    useEffect(() => {
+        setBrand(getUser().brand);
+    }, []);
+
     return (
         <div>
             <div className="row">
                 {
                     menuItems.map((item, i) => (
-                        <div key={i} className="col-lg-3 col-md-4 mb-3">
-                            <Link to={`/reports/${item.url}`}>
-                                <div className='category-box'>
-                                    <div className="category-hover-box">
-                                        <span className='category-name'>{item.title}</span>
+                        <>
+                            {
+                                (brand === "Madeyra" && item.title === "Price List")
+                                    ? null
+                                    : <div key={i} className="col-lg-3 col-md-4 mb-3">
+                                        <Link to={`/reports/${item.url}`}>
+                                            <div className='category-box'>
+                                                <div className="category-hover-box">
+                                                    <span className='category-name'>{item.title}</span>
+                                                </div>
+                                            </div>
+                                        </Link>
                                     </div>
-                                </div>
-                            </Link>
-                        </div>
+                            }
+                        </>
                     ))
                 }
             </div>
