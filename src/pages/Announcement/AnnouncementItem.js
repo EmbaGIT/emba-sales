b7794a9b11dce8af14e96e6a56d2f889579034ac
mtn-faridG <fguluzade@mtnhausdigital.com>
2022-12-04 16:02:21 +0400

    adding loader to announcement pages

--- src/pages/Announcement/AnnouncementItem.js
+++ src/pages/Announcement/AnnouncementItem.js
@@ -3,39 +3,62 @@ import { useParams } from 'react-router-dom'
 import { get } from '../../api/Api'
 import { getHost } from '../../helpers/host'
 import Parser from 'html-react-parser'
+import Loader from 'react-loader-spinner'
 
 const AnnouncementItem = () => {
     const { id } = useParams()
     const [announcement, setAnnouncement] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState('')
 
     useEffect(() => {
         get(`${getHost('announcement', 8093)}/api/announcement/${id}`)
             .then(data => setAnnouncement(data))
-            .catch(err => console.log(err))
+            .catch(err => setError(err))
+            .finally(() => setIsLoading(false))
     }, [])
 
+    useEffect(() => console.log(announcement), [announcement])
+
     return (
-        <div className='row'>
+        <div className='row announcement__item'>
             <div className='col-12'>
-                <img
-                    style={{
-                        height: '320px',
-                        width: '100%',
-                        objectFit: 'cover'
-                    }}
-                    src={announcement.file.url}
-                />
-                <h1>{announcement.title || 'Kampaniya adı mövcud deyil'}</h1>
-                <p>
-                    <strong>{announcement.content || 'Kampaniya məzmunu mövcud deyil'}</strong>
-                </p>
-                <div>
-                    {
-                        announcement.description
-                            ? Parser(announcement.description)
-                            : 'Kampaniyanın ətraflı məzmunu yoxdur'
-                    }
-                </div>
+                { isLoading
+                    ? <div className='w-100 d-flex justify-content-center'>
+                        <Loader
+                            type="ThreeDots"
+                            color="#00BFFF"
+                            height={60}
+                            width={60}
+                        />
+                    </div>
+                    : null
+                }
+                { !isLoading && error ? <div>{error}</div> : null }
+                {
+                    !isLoading && Object.keys(announcement).length && <div className='row'>
+                        <div className='col-lg-6 col-md-6'>
+                            <div className='announcement__item-img'>
+                                <img src={announcement.file?.url} />
+                            </div>
+                        </div>
+                        <div className='col-lg-6 col-md-6'>
+                            <h1 className='announcement__item-title'>{announcement.title || 'Kampaniya adı mövcud' +
+                                ' deyil'}</h1>
+                            <h5 className='announcement__item-content'>
+                                {announcement.content || 'Kampaniya məzmunu mövcud deyil'}
+                            </h5>
+                            <hr />
+                            <div className='announcement__item-description'>
+                                {
+                                    announcement.description
+                                        ? Parser(announcement.description)
+                                        : 'Kampaniyanın ətraflı məzmunu yoxdur'
+                                }
+                            </div>
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     )
--- src/pages/Announcement/AnnouncementList.js
+++ src/pages/Announcement/AnnouncementList.js
@@ -2,28 +2,41 @@ import React, { useEffect, useState } from 'react'
 import { get } from '../../api/Api'
 import { getHost } from '../../helpers/host'
 import { NavLink } from 'react-router-dom'
+import Loader from 'react-loader-spinner'
 
 const AnnouncementList = () => {
     const [activeAnnouncements, setActiveAnnouncements] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState('')
 
     useEffect(() => {
         get(`${getHost('announcement', 8093)}/api/announcement`)
             .then(data => setActiveAnnouncements(data.content.filter(campaign => campaign.status === 'ACTIVE')))
-            .catch(err => console.log(err))
+            .catch(err => setError(err))
+            .finally(() => setIsLoading(false))
     }, [])
 
     return (
         <div className="row">
-            {activeAnnouncements.length && activeAnnouncements.map(announcement => (
-                <div className="col-lg-3 col-md-4 col-sm-12 campaign" key={announcement.id}>
-                    <div className="border-5 rounded overflow-hidden shadow-lg campaign__item">
-                        <div className="campaign__img">
-                            <img
-                                width="100%"
-                                src={announcement.file.url}
-                            />
+            { isLoading
+                ? <div className='w-100 d-flex justify-content-center'>
+                    <Loader
+                        type="ThreeDots"
+                        color="#00BFFF"
+                        height={60}
+                        width={60}
+                    />
+                </div>
+                : null
+            }
+            { !isLoading && error ? <div>{error}</div> : null }
+            { !isLoading && activeAnnouncements.length && activeAnnouncements.map(announcement => (
+                <div className="col-lg-3 col-md-4 col-sm-12 announcement__list" key={announcement.id}>
+                    <div className="border-5 rounded overflow-hidden shadow-lg announcement__list-item">
+                        <div className="announcement__list-img">
+                            <img src={announcement.file?.url} />
                         </div>
-                        <div className="campaign__info p-3">
+                        <div className="announcement__list-info p-3">
                             <h4>{announcement.title || 'Kampaniya adı mövcud deyil'}</h4>
                             <p className="mb-0 d-flex justify-content-between align-content-end">
                                 {announcement.content || 'Kampaniya məzmunu mövcud deyil'}
