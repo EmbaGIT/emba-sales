bc160a1dc332308da797fdb9fd33c063a34c44a9
faridfg <faridfg@code.edu.az>
2023-03-03 16:35:54 +0400

    fetching active status of the order

--- src/pages/OrderHistory/OrderTracking.js
+++ src/pages/OrderHistory/OrderTracking.js
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useState, Fragment } from 'react'
+import Loader from "react-loader-spinner";
+import ReactPaginate from 'react-paginate'
 import { post } from '../../api/Api'
 import { getHost } from '../../helpers/host'
 import AuthContext from '../../store/AuthContext'
-import Loader from "react-loader-spinner";
 
 const OrderTracking = () => {
   const { user_uid } = useContext(AuthContext)
@@ -14,8 +15,12 @@ const OrderTracking = () => {
           .fill(undefined)
           .map((_) => ({ hidden: true }))
   )
+  const [spinners, setSpinners] = useState([])
+  const [gettingActiveStatus, setGettingActiveStatus] = useState(false);
+  const [pageInfo, setPageInfo] = useState({})
+  const [page, setPage] = useState(0)
 
-  const showItems = (i) => {
+  const showItems = (i, status, uid) => {
       setClassNames(classNames.map((name, index) => {
           if (i === index) {
               if (classNames[i].hidden) {
@@ -27,23 +32,54 @@ const OrderTracking = () => {
 
           return name.hidden ? name : {hidden: true}
       }))
+
+      setSpinners(spinners?.map((_, index) => {
+        if (i === index && classNames[i].hidden) {
+          return {show:true}
+        }
+        return {show:false}
+      }))
+
+      if (status === 'IN_PROGRESS' && classNames[i].hidden) {
+        setGettingActiveStatus(true)
+        post(`${getHost('erp/report', 8091)}/api/order-track/specific`, {
+          sales_uid: uid
+        }).then(res => {
+          setGettingActiveStatus(false)
+          setOrders(orders.map(order => {
+            if (order.uid_sales_order !== uid) {
+              return order
+            }
+            return res.sales_group[0]
+          }))
+        })
+      }
   }
 
   useEffect(() => {
     setIsFetching(true)
     // '8f859d20-e5f4-11eb-80d7-2c44fd84f8db'
-    post(`${getHost('erp/report', 8091)}/api/order-track?page=0&size=10`, {
+    post(`${getHost('erp/report', 8091)}/api/order-track?page=${page}&size=3`, {
       uid: '8f859d20-e5f4-11eb-80d7-2c44fd84f8db'
     }).then(res => {
-      setIsFetching(false)
       console.log(res)
+      setIsFetching(false)
+      setSpinners(Array(res?.salesGroups?.length).fill().map(_ => ({show: false})))
+      setPageInfo({
+        totalPages: res.totalPages,
+        totalElements: res.totalElements
+      })
       if (res.hasOwnProperty('info')) {
         setEmptyOrders(res)
       } else {
         setOrders(res?.salesGroups)
       }
     })
-  }, [])
+  }, [page])
+
+  const paginate = (n) => {
+    setPage(+n.selected)
+  }
 
   useEffect(() => console.log(emptyOrders), [emptyOrders])
   useEffect(() => console.log(orders), [orders])
@@ -90,18 +126,30 @@ const OrderTracking = () => {
                 <tbody>
                   {orders?.map((order, i) => (
                     <Fragment key={i}>
-                      <tr key={order.uid_sales_order} onClick={showItems.bind(null, i)} className='parent-row'>
+                      <tr key={order.uid_sales_order} onClick={showItems.bind(null, i, order.statusType, order.uid_sales_order)} className='parent-row'>
                         <td className='long'>{order.sales_order}</td>
                         <td className='short'>{order.customer}</td>
                         <td className='short'>{order.amount}</td>
-                        <td className='long'>
-                          <ol>
-                            {
-                              order.status.info.map((status, i) => (
-                                <li key={i}>{status}</li>
-                              ))
-                            }
-                          </ol>
+                        <td className={`long ${(gettingActiveStatus && spinners[i].show) ? 'text-center' : ''}`}>
+                          {
+                            (gettingActiveStatus && spinners[i].show) ? <div
+                                className='col-12 d-flex justify-content-center w-100'
+                                style={{backdropFilter: 'blur(2px)', zIndex: '100'}}
+                            >
+                                <Loader
+                                    type='ThreeDots'
+                                    color='#00BFFF'
+                                    height={60}
+                                    width={60}
+                                />
+                            </div> : <ol>
+                              {
+                                order.status.info.map((status, i) => (
+                                  <li key={i}>{status}</li>
+                                ))
+                              }
+                            </ol>
+                          }
                         </td>
                         <td className='short'>
                           <span className={`order-tracking-status ${order.statusType === 'READY' ? 'completed' : 'in-progress'}`}>
@@ -145,6 +193,34 @@ const OrderTracking = () => {
                 </tbody>
               </table>
             </div>
+          </div>
+        )
+      }
+
+      {
+        isFetching
+        ? null
+        : (
+          pageInfo?.totalPages > 1 && <div className='d-flex justify-content-center'>
+            <ReactPaginate
+              previousLabel='Əvvəlki'
+              nextLabel='Növbəti'
+              previousClassName='page-item'
+              nextClassName='page-item'
+              previousLinkClassName='page-link'
+              nextLinkClassName='page-link'
+              breakLabel='...'
+              breakClassName='break-me'
+              pageCount={pageInfo.totalPages}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={3}
+              onPageChange={paginate}
+              containerClassName='pagination'
+              activeClassName='active'
+              pageClassName='page-item'
+              pageLinkClassName='page-link'
+              forcePage={page}
+            />
           </div>
         )
       }
