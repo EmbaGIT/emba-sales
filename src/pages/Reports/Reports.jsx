9890de36963f6a5999d8031c4bd8a9af9f9378f8
faridfg <faridfg@code.edu.az>
2023-12-11 17:25:00 +0400

    removed useless imports

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import jwt from "jwt-decode";
-import {get} from "../../api/Api";
 
 const menuItems = [
     {title: 'Mənim Satışlarım', url: 'sales'},
