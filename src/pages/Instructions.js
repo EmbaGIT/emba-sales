77039462af9b4d12a12229a7ec639a1e7de65c5e
faridfg <faridfg@code.edu.az>
2024-06-12 16:21:29 +0400

    videos related with leobank installment

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -8,7 +8,15 @@ const videos = [
     {
         link: 'https://www.youtube.com/embed/FxLvwBuCD6c',
         title: 'Emba.Store_Ucotcular_1C'
-    }
+    },
+    {
+        link: 'https://www.youtube.com/embed/su4XigfTCJg',
+        title: 'Leo Sifarish 2'
+    },
+    {
+        link: 'https://www.youtube.com/embed/u7e0b11u3Xk',
+        title: 'Leo GeriQaytarma'
+    },
 ]
 
 const Instructions = () => {
@@ -16,7 +24,7 @@ const Instructions = () => {
         <div className='row'>
             {
                 videos.map(video => (
-                    <div className='col-lg-6 col-md-6 col-sm-12 instruction' key={video.link}>
+                    <div className='col-lg-6 col-md-6 col-sm-12 mb-3 instruction' key={video.link}>
                         <div className='instruction__video shadow-lg'>
                             <iframe
                                 src={video.link}
